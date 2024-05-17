6f57e988b87b48cf1d1c9d0faa1fd72d1f0312fc
Akhilesh Patel <akhil2109kumar@gmail.com>
2024-05-17 16:28:56 +0530

    handled response from the api by which we are fetching dog images with breed

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,7 +2,7 @@ class DogsController < ApplicationController
 
   def create
     dog = find_or_initialize_dog
-    if dog.save
+    if dog.present?
       render json: { breed: dog.breed, image_url: dog.image_url }
     else
       render json: { error: 'Dog breed not found' }, status: :unprocessable_entity
@@ -16,10 +16,24 @@ class DogsController < ApplicationController
   end
 
   def find_or_initialize_dog
-    Dog.find_or_initialize_by(breed: formatted_breed) do |dog|
-      dog.assign_attributes(dog_params)
-      dog.image_url = DogImageFetcher.fetch_dog_image_by_breed(formatted_breed)
-    end
+    dog = Dog.find_by(breed: formatted_breed)
+    return dog if dog.present?
+    create_dog_with_image
+  end
+
+  def create_dog_with_image
+    response = DogImageFetcher.fetch_dog_image_by_breed(formatted_breed)
+    return unless response['status'] == 'success'
+
+    Dog.create(breed: formatted_breed, image_url: response['message'])
+  rescue RestClient::NotFound => e
+    handle_error(e)
+    nil
+  end
+
+  def handle_error(error)
+    logger.error("Error fetching dog image: #{error.message}")
+    raise ActiveRecord::Rollback, error.response&.body || "Invalid Breed"
   end
 
   def formatted_breed
--- app/services/dog_image_fetcher.rb
+++ app/services/dog_image_fetcher.rb
@@ -4,30 +4,12 @@ class DogImageFetcher
   def self.fetch_dog_image_by_breed(breed)
     response = RestClient.get(BASE_URL + "#{breed}/images/random")
     handle_response(response)
-  rescue RestClient::NotFound => e
-    ActiveRecord::Base.transaction do
-
-    error_message = e.response&.body || "Invalid Breed"
-    JSON.parse(error_message)['message']
-    handle_error("Dog breed not found: #{JSON.parse(error_message)['message']}")
-    raise ActiveRecord::Rollback
-    end
-  rescue RestClient::ExceptionWithResponse => e
-    handle_error("Dog API request failed: #{e.response.body}")
-  rescue StandardError => e
-    handle_error("Error while fetching dog image: #{e.message}")
   end
 
   private
 
   def self.handle_response(response)
-    if response.code == 200
-      JSON.parse(response.body)['message']
-    end
-  end
-
-  def self.handle_error(message)
-    Rails.logger.error(message)
+    JSON.parse(response)
   end
 end
 
